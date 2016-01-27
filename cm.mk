c4787cbcf9575eb73651f3b48da7f340285d953f
Fabian <ruffy91@users.noreply.github.com>
2016-01-27 20:19:28 +0100

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,12 +1,12 @@
 # Release name
-PRODUCT_RELEASE_NAME := t0lte
+PRODUCT_RELEASE_NAME := u0lte
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/t0lte/full_t0lte.mk)
+$(call inherit-product, device/samsung/u0lte/full_u0lte.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
@@ -15,11 +15,11 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := t0lte
-PRODUCT_NAME := cm_t0lte
+PRODUCT_DEVICE := u0lte
+PRODUCT_NAME := cm_u0lte
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := GT-N7105
+PRODUCT_MODEL := EK-GN120
 PRODUCT_MANUFACTURER := samsung
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t0lte TARGET_DEVICE=t0lte BUILD_FINGERPRINT="samsung/t0ltexx/t0lte:4.4.2/KOT49H/N7105XXUFOB2:user/release-keys" PRIVATE_BUILD_DESC="t0ltexx-user 4.4.2 KOT49H N7105XXUFOB2 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=u0lte TARGET_DEVICE=u0lte BUILD_FINGERPRINT="samsung/u0ltexx/u0lte:4.2.2/JDQ39/GN120XXUAND3:user/release-keys" PRIVATE_BUILD_DESC="u0ltexx-user 4.2.2 JDQ39 GN120XXUAND3 release-keys"
