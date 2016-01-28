7b07cb3aed791a3d92ffded68287de74d4b31b86
fabian <fabian@android-dev>
2016-01-28 14:54:56 +0100

    Gives a working recovery

---
+++ BoardCommonConfig.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This variable is set first, so it can be overridden
+# by BoardConfigVendor.mk
+
+LOCAL_PATH := device/samsung/u0lte
+
+# RIL
+COMMON_GLOBAL_CFLAGS += -DPROPERTY_PERMS_APPEND='{ "ril.ks.status", AID_SYSTEM, 0 },'
+
+# Recovery
+TARGET_RECOVERY_FSTAB := device/samsung/t0lte/rootdir/fstab.smdk4x12
+RECOVERY_FSTAB_VERSION := 2
+
+# assert
+TARGET_OTA_ASSERT_DEVICE := u0lte,u0ltexx,EK-GN120
+
+TARGET_PREBUILT_KERNEL := device/samsung/u0lte/kernel
+
+
+
--- cm.mk
+++ cm.mk
@@ -1,18 +1,26 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/cm/config/common.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := u0lte
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Screen Size
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/u0lte/device_u0lte.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := u0lte
 PRODUCT_NAME := cm_u0lte
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := u0lte
+PRODUCT_MODEL := EK-GN120
 PRODUCT_MANUFACTURER := samsung
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=u0lte TARGET_DEVICE=u0lte BUILD_FINGERPRINT="samsung/u0ltexx/u0lte:4.2.2/release-keys" PRIVATE_BUILD_DESC="u0ltexx-user 4.2.2 release-keys"
+
